91f14ff5cc05aaa7c0a8a2d34246f4555682ea05
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2020-02-12 11:07:41 +0530

    Initial commit : Array adt has been created and is implemented by Array class

---
+++ src/adt/arrayadt.java
@@ -0,0 +1,10 @@
+package adt;
+
+public interface arrayadt
+{
+    int[] traverseArray(int[] arr );
+    int[] printArray(int[] arr);
+    void searchInArray(int [] arr,int value);
+    void insertIntoArray(int[] arr, int value);
+
+}
---
+++ src/definition/Array.java
@@ -0,0 +1,46 @@
+package definition;
+
+import adt.arrayadt;
+
+public class Array implements arrayadt {
+    @Override
+    public int[] traverseArray(int[] arr)
+    {
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println("the element we are traversing is "+arr[i]);
+
+        }
+        return new int[0];
+    }
+
+    @Override
+    public int[] printArray(int[] arr) {
+        for (int j = 0; j <arr.length ; j++)
+        {
+            System.out.println("the array elements are "+arr[j]);
+
+        }
+        return new int[0];
+    }
+
+    @Override
+    public void searchInArray(int[] arr, int value) {
+        for (int i = 0; i <arr.length ; i++)
+        {
+            if(arr[i]==value)
+            {
+                System.out.println("value found at "+i+1);
+            }
+            else
+            {
+                System.out.println("value not found in the array");
+            }
+            
+        }
+    }
+
+    @Override
+    public void insertIntoArray(int[] arr, int value) {
+
+    }
+}
