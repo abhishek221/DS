77f27a117b019e5da49fd6da23e159f8ff7fb422
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2020-02-16 23:17:46 +0530

    Initial commit

---
+++ LAB04/src/Node/Node.java
@@ -0,0 +1,7 @@
+package Node;
+
+public class Node {
+    int data;
+    Node next;
+
+}
---
+++ LAB04/src/Stack/Stack.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import Node.Node;
+
+public class Stack {
+    Node head;
+    Stack()
+    {
+        head = null;
+
+    }
+
+    void push(int data)
+    {
+        if(head==null)
+        {
+            
+        }
+    }
+
+}
