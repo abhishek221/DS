2cdcaadb8aa97f338b35f8db849beaff5cdba316
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2020-04-02 00:35:24 +0530

    init commit : BubbleSort class created

---
+++ Sorting/src/bubblesort/BubbleSort.java
@@ -0,0 +1,16 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: ABHISHEK
+ *  Date: 02-04-2020
+ *  Time: 00:32
+ */
+package bubblesort;
+
+public class BubbleSort
+{
+    public static int[] bubbleSort(int[] array)
+    {
+        return array;
+
+    }
+}
---
+++ Sorting/src/main/Main.java
@@ -0,0 +1,39 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: ABHISHEK
+ *  Date: 02-04-2020
+ *  Time: 00:15
+ */
+package main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the sorting menu ");
+        System.out.println("Please enter the size of array ");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        int[] randomNumbers = new int[size];
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i]= (int)(Math.random() *100);
+
+        }
+
+        System.out.println("The array you entered is ");
+        System.out.println(Arrays.toString(randomNumbers));
+
+        System.out.println("how would you want to sort your array");
+        System.out.println("press 1 for bubble sort");
+        int response = scanner.nextInt();
+
+        switch (response){
+            case 1:
+
+        }
+
+    }
+}
