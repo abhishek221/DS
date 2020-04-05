aa2daf3858af4bd38fb76b8389ee54975f1e8760
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2020-04-05 21:41:18 +0530

    bubbleSort method working fine

--- Sorting/src/bubblesort/BubbleSort.java
+++ Sorting/src/bubblesort/BubbleSort.java
@@ -8,9 +8,33 @@ package bubblesort;
 
 public class BubbleSort
 {
-    public static int[] bubbleSort(int[] array)
+    public static void bubbleSort(int[] array)
     {
-        return array;
+        for (int i = array.length-1; i >0 ; i--) {
+            for (int j = 0; j <i ; j++) {
+                if(array[j]>array[j+1])
+                {
+                    swap(array,j,j+1);
+                }
+                
+            }
+
+        }
+        for (int i = 0; i <array.length ; i++) {
+            System.out.print(array[i]+", ");
+
+        }
+
+
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        if(i==j){
+            return;}
+
+        int temp = array[i];
+        array[i]=array[j];
+        array[j]=temp;
 
     }
 }
